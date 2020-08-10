0181d4c8942b39d9fe4775bea99803dfda3afe58
Paulo Carvalho <pauloej.carvalho@gmail.com>
2020-08-10 10:11:04 -0300

    Regras do compilador e opções

---
+++ compilador/compilador.ts
@@ -0,0 +1,25 @@
+let canal: string = 'Gaveta'
+let inscritos: number = 612333
+
+// canal = inscritos
+console.log(`Canal = ${canal}`)
+
+// let nome = 'Paulo'
+
+function soma(a: any, b: any) {
+  return a + b
+}
+
+let qualquerCoisa
+qualquerCoisa = 12
+qualquerCoisa = 'abc'
+
+function saudar(isManha: boolean): string {
+  let saudacao: string
+  if(isManha) {
+    saudacao = 'Bom dia!'
+  } else {
+    saudacao = 'Boa vida!'
+  }
+  return saudacao
+}
--- tipos/tipos.js
+++ tipos/tipos.js
@@ -1,29 +1,29 @@
 "use strict";
 // string
-var nome = 'Paulo';
+let nome = 'Paulo';
 console.log(nome);
 // nome = 28
 // numbers
-var idade = 27;
+let idade = 27;
 // idade = 'Ana'
 idade = 27.5;
 console.log(idade);
 // boolean
-var possuiHobbies = false;
+let possuiHobbies = false;
 // possuiHobbies = 1
 console.log(possuiHobbies);
 // tipos explícitos
-var minhaIdade;
+let minhaIdade;
 minhaIdade = 27;
 console.log(typeof minhaIdade);
 // array
-var hobbies = ["Jogar Games", "Praticar Esportes"];
+let hobbies = ["Jogar Games", "Praticar Esportes"];
 console.log(hobbies[0]);
 console.log(typeof hobbies);
 hobbies = [100];
 console.log(hobbies);
 // Tuplas - Array com uma quantidade pré definida de tipos
-var endereco = ["Av Principal", 99, "CASA 1"];
+let endereco = ["Av Principal", 99, "CASA 1"];
 console.log(endereco);
 endereco = ["Rua importante", 1206, "Bloco C"];
 console.log(endereco);
@@ -34,11 +34,11 @@ var Cor;
     Cor[Cor["Verde"] = 100] = "Verde";
     Cor[Cor["Azul"] = 2] = "Azul";
 })(Cor || (Cor = {}));
-var minhaCor = Cor.Verde;
+let minhaCor = Cor.Verde;
 console.log(minhaCor);
 console.log(Cor.Azul);
 // any
-var carro = 'BMW';
+let carro = 'BMW';
 console.log(carro);
 carro = { marca: 'BMW', ano: 2020 };
 console.log(carro);
@@ -59,13 +59,13 @@ function multiplicar(numA, numB) {
 // console.log(multiplicar(2, 'Bia'))
 console.log(multiplicar(2, 9));
 // Tipo função
-var calculo;
+let calculo;
 // calculo = digaOi
 // calculo()
 calculo = multiplicar;
 console.log(calculo(5, 6));
 // Objetos
-var usuario = {
+let usuario = {
     nome: 'Paulo',
     idade: 27
 };
@@ -81,9 +81,9 @@ usuario = {
     nome: 'Maria',
 };
 console.log(usuario);
-var funcionario = {
+let funcionario = {
     supervisores: ['Paulo', 'Mayara'],
-    baterPonto: function (horario) {
+    baterPonto(horario) {
         if (horario <= 8) {
             return 'Ponto normal';
         }
@@ -92,9 +92,9 @@ var funcionario = {
         }
     }
 };
-var funcionario2 = {
+let funcionario2 = {
     supervisores: ['Bia', 'Ana'],
-    baterPonto: function (horario) {
+    baterPonto(horario) {
         if (horario <= 8) {
             return 'Ponto normal';
         }
@@ -107,12 +107,12 @@ console.log(funcionario.supervisores);
 console.log(funcionario.baterPonto(8));
 console.log(funcionario.baterPonto(9));
 // Union Types
-var nota = 10;
-console.log("Minha nota \u00E9 " + nota + "!");
+let nota = 10;
+console.log(`Minha nota é ${nota}!`);
 nota = '10';
-console.log("Minha nota \u00E9 " + nota + "!");
+console.log(`Minha nota é ${nota}!`);
 // Checando tipos
-var valor = 30;
+let valor = 30;
 if (typeof valor === "number") {
     console.log("É um number!");
 }
@@ -123,10 +123,10 @@ else {
 function falha(msg) {
     throw new Error(msg);
 }
-var produto = {
+const produto = {
     nome: 'Sabão',
     preco: 4,
-    validarProduto: function () {
+    validarProduto() {
         if (!this.nome || this.nome.trim().length == 0) {
             falha("Precisa ter um nome!");
         }
@@ -137,11 +137,11 @@ var produto = {
 };
 produto.validarProduto();
 // Valores opcionais com tipo Nulo
-var altura = 12;
+let altura = 12;
 // altura = null
-var alturaOpcional = 12;
+let alturaOpcional = 12;
 alturaOpcional = null;
-var contato1 = {
+const contato1 = {
     nome: 'Fulano',
     tel1: '984545411',
     tel2: null
@@ -149,16 +149,17 @@ var contato1 = {
 console.log(contato1.nome);
 console.log(contato1.tel1);
 console.log(contato1.tel2);
-var contaBancaria = {
+let contaBancaria = {
     saldo: 3456,
-    depositar: function (valor) {
+    depositar(valor) {
         this.saldo += valor;
     }
 };
-var correntista = {
+let correntista = {
     nome: 'Ana Silva',
     contaBancaria: contaBancaria,
     contatos: ['34567890', '98765432']
 };
 correntista.contaBancaria.depositar(3000);
 console.log(correntista);
+//# sourceMappingURL=tipos.js.map
