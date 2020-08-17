97fc08c0f28702d28e01918b6733661adf9acda1
Paulo Carvalho <pauloej.carvalho@gmail.com>
2020-08-16 21:24:30 -0300

    Modos diferentes de importação de módulos

--- modulos/modulos.js
+++ modulos/modulos.js
@@ -1,8 +1,15 @@
 "use strict";
+// import { areaRetangulo } from './retangulo'
+// import { areaCircunferencia } from './circunferencia'
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const retangulo_1 = require("./retangulo");
+// Modos diferentes de importação e exportação
+// Função exportada no modo default não exige necessidade de ser o mesmo nome da função
+const retangulo_1 = __importDefault(require("./retangulo"));
 const circunferencia_1 = require("./circunferencia");
 console.log('Módulo carregado...');
-console.log(retangulo_1.areaRetangulo(7, 8));
+console.log(retangulo_1.default(7, 8));
 console.log(circunferencia_1.areaCircunferencia(2));
 //# sourceMappingURL=modulos.js.map
--- modulos/modulos.ts
+++ modulos/modulos.ts
@@ -1,6 +1,11 @@
-import { areaRetangulo } from './retangulo'
-import { areaCircunferencia } from './circunferencia'
+// import { areaRetangulo } from './retangulo'
+// import { areaCircunferencia } from './circunferencia'
+
+// Modos diferentes de importação e exportação
+// Função exportada no modo default não exige necessidade de ser o mesmo nome da função
+import retangulo from './retangulo'
+import { areaCircunferencia as circ } from './circunferencia'
 
 console.log('Módulo carregado...')
-console.log(areaRetangulo(7, 8))
-console.log(areaCircunferencia(2))
+console.log(retangulo(7, 8))
+console.log(circ(2))
