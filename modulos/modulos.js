57dbe00051fc1c18ab1c62084adc68036b578301
Paulo Carvalho <pauloej.carvalho@gmail.com>
2020-08-16 21:32:44 -0300

    Utilizando padrão CommonsJS

--- modulos/modulos.js
+++ modulos/modulos.js
@@ -12,4 +12,6 @@ const circunferencia_1 = require("./circunferencia");
 console.log('Módulo carregado...');
 console.log(retangulo_1.default(7, 8));
 console.log(circunferencia_1.areaCircunferencia(2));
+const { digaOi } = require('./novo');
+console.log(digaOi('Paulo'));
 //# sourceMappingURL=modulos.js.map
---
+++ modulos/novo.js
@@ -0,0 +1,7 @@
+"use strict";
+module.exports = {
+    digaOi(nome) {
+        return "Olá!" + nome;
+    }
+};
+//# sourceMappingURL=novo.js.map
