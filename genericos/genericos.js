e48e2a2927e5468dd74fad9bcad4406a15b363b8
Paulo Carvalho <pauloej.carvalho@gmail.com>
2020-08-22 18:09:20 -0300

    Generics com Array

--- genericos/genericos.js
+++ genericos/genericos.js
@@ -12,4 +12,10 @@ function echoMelhorado(objeto) {
 console.log(echoMelhorado('João').length);
 console.log(echoMelhorado(27));
 console.log(echoMelhorado({ nome: 'Paulo', idade: 27 }));
+// Generics disponíveis na API
+// A classe Array foi criada de uma forma genérica na API JS, porém podemos atribuir tipos nesse generics
+const avaliacoes = [10, 9.3, 6.7];
+avaliacoes.push(8.4);
+//avaliacoes.push('5.5') -- Não consegue dar um push em um Array genérico do tipo number
+console.log(avaliacoes);
 //# sourceMappingURL=genericos.js.map
--- genericos/genericos.ts
+++ genericos/genericos.ts
@@ -13,3 +13,10 @@ function echoMelhorado<T>(objeto: T): T {
 console.log(echoMelhorado('João').length)
 console.log(echoMelhorado<number>(27))
 console.log(echoMelhorado({nome: 'Paulo', idade: 27}))
+
+// Generics disponíveis na API
+// A classe Array foi criada de uma forma genérica na API JS, porém podemos atribuir tipos nesse generics
+const avaliacoes: Array<number> = [10, 9.3, 6.7]
+avaliacoes.push(8.4)
+//avaliacoes.push('5.5') -- Não consegue dar um push em um Array genérico do tipo number
+console.log(avaliacoes)
